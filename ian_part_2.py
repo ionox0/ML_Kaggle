d4bc05b37f9dfe3a2a1a247dac073af6b2e6bb5e
ionox0 <ionox0@gmail.com>
2016-04-10 20:05:47 -0400

    voting classifier, + combining numerical and categorical classifiers

--- ian_part_2.py
+++ ian_part_2.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[63]:
+# In[23]:
 
 import csv
 import math
@@ -11,8 +11,10 @@ import string
 
 # Classification utils
 from sklearn.cross_validation import train_test_split
+from sklearn.cross_validation import cross_val_score
 from sklearn.feature_selection import VarianceThreshold
 from sklearn.preprocessing import LabelEncoder
+from sklearn.cross_validation import KFold
 
 # Classifiers
 from sklearn.neighbors import KNeighborsClassifier
@@ -20,6 +22,8 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB, MultinomialNB
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+
+from sklearn.ensemble import VotingClassifier
 
 
 # In[2]:
@@ -29,7 +33,7 @@ train = pandas.read_csv('data.csv')
 test = pandas.read_csv('quiz.csv')
 
 
-# In[3]:
+# In[20]:
 
 # Name Columns (53 total)
 alphabet = list(string.ascii_lowercase)
@@ -75,15 +79,17 @@ for i, letter in enumerate(alphabet2):
 # [1, 6, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33,
 # 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 47, 48, 49, 50, 51, 52]
 
+train_labels = np.array(train['aaa']).astype(int)
 
-# In[34]:
+
+# In[ ]:
 
 sel = VarianceThreshold(threshold=(.8))
 reduced_features_train = sel.fit_transform(train[numeric_cols[:-1]])
 print(reduced_features_train)
 
 
-# In[44]:
+# In[14]:
 
 # Features shown to have coorelation with label
 coorelated_features = ['q', 'aa', 'bb', 'vv', 'ww']
@@ -91,34 +97,31 @@ coorelated_features = ['q', 'aa', 'bb', 'vv', 'ww']
 rfe_selected_numeric_cols = ['x', 'p', 'dd', 'kk']
 
 
-# In[85]:
+# In[7]:
 
 def cross_val(clf, train_data, train_labels):
-    error_rates = []
-    for i in range(10):
-        # todo - kfold cross val
-        x_train, x_test, y_train, y_test = train_test_split(train_data, train_labels, test_size=0.4)
-        clf_trained = clf.fit(x_train, y_train)
-        e = clf_trained.score(x_test, y_test)
-        error_rates.append(e)
-    
-    return np.mean(error_rates)
+    x_train, x_test, y_train, y_test = train_test_split(train_data, train_labels, test_size=0.4)
+    clf_trained = clf.fit(x_train, y_train)
+    scores = cross_val_score(clf_trained, x_train, y_train, cv=2)
+    return scores
 
 
-# In[89]:
+# In[18]:
 
 # Training error using numeric columns
-labels = np.array(train['aaa']).astype(int)
+
 # Keep label out of features
-e = cross_val(DecisionTreeClassifier, train[coorelated_features], labels)
+clf = KNeighborsClassifier()
+e = cross_val(clf, train[coorelated_features], train_labels)
 print(e)
 
 
-# In[91]:
+# In[12]:
 
 # Training error using categorical columns
 
 # Method to convert to one-hot encodings
+# pd.get_dummies --> returns matrix of every feature, concatenated from all cols, into one feature space
 def encode_as_labels(X):
     output = X.copy()
     if X.columns is not None:
@@ -129,15 +132,32 @@ def encode_as_labels(X):
             output[colname] = LabelEncoder().fit_transform(col)
     return output
 
+# kf = KFold(4, n_folds=2, shuffle=True)
+
 enc_train = encode_as_labels(train[categorical_cols])
 
-labels = np.array(train['aaa']).astype(int)
-r = RandomForestClassifier(n_estimators=100)
-e = cross_val(r, enc_train, labels)
+r = RandomForestClassifier(n_estimators=100, max_depth=5)
+
+e = cross_val(r, enc_train, train_labels)
 print(e)
 
 
-# In[53]:
+# In[21]:
+
+# Combine numerical and categorical classifiers
+x_train, x_test, y_train, y_test = train_test_split(train_data, train_labels, test_size=0.4)
+
+num_clf = KNeighborsClassifier()
+num_clf_trained = num_clf.fit(train[coorelated_features], train_labels)
+
+enc_train = encode_as_labels(train[categorical_cols])
+cat_clf = RandomForestClassifier(n_estimators=100, max_depth=5)
+cat_clf_trained = cat_clf.fit(enc_train, train_labels)
+
+scores = cross_val_score(num_clf_trained, x_train, y_train, cv=2)
+
+
+# In[17]:
 
 # Make predictions using numeric columns
 trained = KNeighborsClassifier().fit(train[numeric_cols[:-1]], train['aaa'])
@@ -145,19 +165,23 @@ preds = trained.predict(np.array(test[numeric_cols[:-1]]))
 write_results(preds)
 
 
-# In[84]:
+# In[ ]:
 
 # Make predictions using categorical columns
 enc_train = encode_as_labels(train[categorical_cols])
-labels = np.array(train['aaa']).astype(int)
-trained = RandomForestClassifier(n_estimators=100).fit(enc_train, labels)
+frames = [train[numeric_cols], enc_train]
+result = pandas.concat(frames, axis=1)
 
-enc_test = encode_as_labels(test[categorical_cols])
-preds = trained.predict(enc_test)
-write_results(preds)
+clf1 = LogisticRegression(random_state=1)
+clf2 = RandomForestClassifier(random_state=1, max_depth=5)
+clf3 = GaussianNB()
+
+eclf = VotingClassifier([('lr', clf1), ('rf', clf2), ('gnb', clf3)], voting='hard')
+scores = cross_val_score(eclf, result, labels, cv=10, scoring='accuracy')
+scores
 
 
-# In[13]:
+# In[10]:
 
 def write_results(preds):
     with open('test_predictions.csv', 'wb') as csvfile:
@@ -165,6 +189,26 @@ def write_results(preds):
         writer.writerow(['id', 'Prediction'])
         for i, pred in enumerate(preds):
             writer.writerow([i+1, pred])
+
+
+# In[36]:
+
+clf1 = LogisticRegression(random_state=1)
+clf2 = RandomForestClassifier(random_state=1, max_depth=5)
+clf3 = GaussianNB()
+
+eclf = VotingClassifier([('lr', clf1), ('rf', clf2), ('gnb', clf3)], voting='soft', weights=[1,1,1])
+
+enc_train = encode_as_labels(train[categorical_cols])
+frames = [train[numeric_cols[:-1]], enc_train]
+train_result = pandas.concat(frames, axis=1)
+
+enc_test = encode_as_labels(test[categorical_cols])
+frames = [test[numeric_cols[:-1]], enc_test]
+test_result = pandas.concat(frames, axis=1)
+
+eclf.fit(train_result, train['aaa'])
+write_results(eclf.predict(test_result))
 
 
 # In[ ]:
